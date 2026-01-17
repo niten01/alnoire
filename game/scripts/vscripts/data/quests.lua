80413e9e18e4a306230016ef6ecba9bab70752e9
icesiski <kole4kiprime@mail.ru>
2026-01-17 20:53:04 +0400

    Kfc4

--- game/scripts/vscripts/data/quests.lua
+++ game/scripts/vscripts/data/quests.lua
@@ -1,24 +1,42 @@
 return {
-    q_test_quest = {
+    q_clash_royal = {
         giver = "npc_tiny",
         name = "Королевская взбучка",
         steps = {
             {
-                description = "Иди на арену.",
+                description = "Иди на арену",
                 objectives = {
                     { type = "come", trigger = "trigger_clash_royal" }
                 }
             },
             {
-                description = "Уничтожь владения Красного Принца.",
+                description = "Уничтожь владения Красного Принца",
                 objectives = {
                     { type = "kill", npc = "npc_tron" }
                 }
             },
             {
-                description = "Вернись к Синему Принцу.",
+                description = "Вернись к Синему Принцу",
                 objectives = {
                     { type = "talk", npc = "npc_blue_prince" }
+                }
+            },
+        }
+    },
+     q_nigger = {
+        giver = "npc_nigg",
+        name = "O'Block",
+        steps = {
+            {
+                description = "Добудь билеты у перекупа",
+                objectives = {
+                    { type = "take", trigger = "trigger_ticket" }
+                }
+            },
+            {
+                description = "Отдай билет крипу",
+                objectives = {
+                    { type = "talk", npc = "npc_nigg" }
                 }
             },
         }
