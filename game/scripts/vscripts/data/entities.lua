bb58f2f8b6e4b6532cdac61fc44591df4399c193
niten01 <ovcharov_05@mail.ru>
2026-02-16 22:49:54 +0300

    ogre endless fight

--- game/scripts/vscripts/data/entities.lua
+++ game/scripts/vscripts/data/entities.lua
@@ -404,6 +404,7 @@ return {
         },
         pack_ogre_bruiser = {
             activateAfterUnitsSpawned = false,
+            stayActivatedOnPlayerDeath = true,
         },
         pack_gorilla = {
             activateAfterUnitsSpawned = false,
