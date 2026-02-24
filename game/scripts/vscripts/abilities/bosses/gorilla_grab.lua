2efecc7da9fe3f3138a928d3433afbe70b326e19
niten01 <ovcharov_05@mail.ru>
2026-02-24 20:53:32 +0300

    gorilla grab

---
+++ game/scripts/vscripts/abilities/bosses/gorilla_grab.lua
@@ -0,0 +1,21 @@
+gorilla_grab = class {}
+
+function gorilla_grab:ShowWarning()
+  local caster = self:GetCaster()
+  local delay = self:GetSpecialValueFor("warning_delay")
+  local radius = self:GetSpecialValueFor("radius")
+  ShowGenericCircleWarning(caster:GetAbsOrigin(), radius, delay)
+  return delay
+end
+
+function gorilla_grab:OnSpellStart()
+  local caster = self:GetCaster()
+  local radius = self:GetSpecialValueFor("radius")
+  local duration = self:GetSpecialValueFor("umm_duration")
+  local damage = self:GetSpecialValueFor("damage")
+  local enemies = FindEnemiesForAIInRadius(caster:GetAbsOrigin(), radius)
+  for _, ent in ipairs(enemies) do
+    ent:AddNewModifier(caster, self, "modifier_gorilla_umm", { duration = duration, damage = damage })
+    break
+  end
+end
