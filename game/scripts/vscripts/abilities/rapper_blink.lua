574c83a1227592d69aaffb2d7e88b51f3417c2d2
niten01 <ovcharov_05@mail.ru>
2026-02-28 02:05:44 +0300

    rapper souljah

--- game/scripts/vscripts/abilities/rapper_blink.lua
+++ game/scripts/vscripts/abilities/rapper_blink.lua
@@ -1,4 +1,5 @@
 rapper_blink = class {}
+LinkLuaModifier("modifier_rapper_postblink", "abilities/rapper_blink.lua", LUA_MODIFIER_MOTION_NONE)
 
 function rapper_blink:OnSpellStart()
     if not IsServer() then return end
@@ -9,11 +10,13 @@ function rapper_blink:OnSpellStart()
     local maxDistance = self:GetSpecialValueFor("range")
     local destination = GetSafeBlinkDestination(casterPos, targetPos, maxDistance)
 
-    local pfx         = ParticleManager:CreateParticle(
+    EmitSoundOnLocationWithCaster(casterPos, "ability.rapper.blink.from", caster)
+    local pfx = ParticleManager:CreateParticle(
         "particles/econ/events/fall_2022/blink/blink_dagger_fall_2022_start.vpcf", PATTACH_WORLDORIGIN, nil)
     ParticleManager:SetParticleControl(pfx, 0, casterPos)
     ParticleManager:ReleaseParticleIndex(pfx)
 
+    EmitSoundOnLocationWithCaster(destination, "ability.rapper.blink.to", caster)
     pfx = ParticleManager:CreateParticle("particles/econ/events/fall_2022/blink/blink_dagger_end_fall2022.vpcf",
         PATTACH_WORLDORIGIN, nil)
     ParticleManager:SetParticleControl(pfx, 0, destination)
@@ -28,6 +31,10 @@ function rapper_blink:OnSpellStart()
 
     FindClearSpaceForUnit(caster, destination, true)
     ProjectileManager:ProjectileDodge(caster)
+
+    caster:AddNewModifier(caster, self, "modifier_rapper_postblink", {
+        duration = self:GetSpecialValueFor("buff_duration")
+    })
 end
 
 function rapper_blink:GetCastRange(vLocation, hTarget)
@@ -35,4 +42,46 @@ function rapper_blink:GetCastRange(vLocation, hTarget)
         return self:GetSpecialValueFor("range")
     end
     return 0
+end
+
+--------------------------------------------------
+
+modifier_rapper_postblink = class {}
+
+function modifier_rapper_postblink:IsHidden() return false end
+
+function modifier_rapper_postblink:IsDebuff() return false end
+
+function modifier_rapper_postblink:IsPurgable() return false end
+
+function modifier_rapper_postblink:OnCreated()
+    if not IsServer() then return end
+
+    local parent = self:GetParent()
+    self.pfx = ParticleManager:CreateParticle("particles/rapper_blink_buff.vpcf",
+        PATTACH_ABSORIGIN_FOLLOW, parent)
+
+    self.msReduction = self:GetAbility():GetSpecialValueFor("ms_reduction")
+    self.attackRangeBonus = self:GetAbility():GetSpecialValueFor("attack_range_bonus")
+end
+
+function modifier_rapper_postblink:OnDestroy()
+    if not IsServer() then return end
+    ParticleManager:DestroyParticle(self.pfx, false)
+    ParticleManager:ReleaseParticleIndex(self.pfx)
+end
+
+function modifier_rapper_postblink:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+        MODIFIER_PROPERTY_ATTACK_RANGE_BONUS,
+    }
+end
+
+function modifier_rapper_postblink:GetModifierMoveSpeedBonus_Constant()
+    return -self.msReduction
+end
+
+function modifier_rapper_postblink:GetModifierAttackRangeBonus()
+    return self.attackRangeBonus
 end
