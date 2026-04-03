e3733947dbfde584ca6d19fe085f1075d006895e
icesiski <kole4kiprime@mail.ru>
2026-04-03 18:46:56 +0400

    kfc

--- game/scripts/vscripts/data/entities.lua
+++ game/scripts/vscripts/data/entities.lua
@@ -1301,9 +1301,9 @@ return {
         },
 
         pack_darkforest_act3_treant = {
-            rangeFastTickRate = 2200,
-            rangeRetreat = 1200,
-            rangeAggro = 900,
+            rangeFastTickRate = 2400,
+            rangeRetreat = 1300,
+            rangeAggro = 950,
             stayActivatedOnPlayerDeath = true,
         },
 
