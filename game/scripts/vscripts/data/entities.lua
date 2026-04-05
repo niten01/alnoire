c890eafabc2c44ec742bf6edbffb0e0c3222596a
Egor <egorskorik398@gmail.com>
2026-04-06 01:32:57 +0300

    chaser final wip

--- game/scripts/vscripts/data/entities.lua
+++ game/scripts/vscripts/data/entities.lua
@@ -417,7 +417,7 @@ return {
             team = DOTA_TEAM_BADGUYS,
         },
         spawner_chaser                = {
-            npc = "npc_chaser",
+            npc = "npc_chaser_final",
             packID = "pack_chaser",
             ai_modifier = "modifier_default_creep_ai",
             team = DOTA_TEAM_BADGUYS,
