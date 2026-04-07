84be0a7289c47e7b81054db434847e5ae23cc804
Egor <egorskorik398@gmail.com>
2026-04-07 16:01:11 +0300

    mk passive

--- game/scripts/vscripts/abilities/bosses/mk_passive_custom.lua
+++ game/scripts/vscripts/abilities/bosses/mk_passive_custom.lua
@@ -12,6 +12,7 @@ end
 modifier_mk_passive_stacks = class({})
 
 function modifier_mk_passive_stacks:IsHidden() return false end
+
 function modifier_mk_passive_stacks:IsPurgable() return false end
 
 function modifier_mk_passive_stacks:DeclareFunctions()
@@ -24,30 +25,34 @@ function modifier_mk_passive_stacks:OnAttackLanded(params)
     if not IsServer() then return end
     if params.attacker ~= self:GetParent() then return end
     if params.target:IsBuilding() or params.target:IsOther() then return end
-
+    local abil = self:GetAbility()
+    local maxStacks = abil:GetSpecialValueFor('max_stacks')
     local target = params.target
     local modifier_name = "modifier_mk_stack_debuff"
     local mod = target:AddNewModifier(self:GetParent(), self:GetAbility(), modifier_name, { duration = 10 })
     if mod then
-        if mod:GetStackCount() < 8 then
+        if mod:GetStackCount() < maxStacks then
             mod:IncrementStackCount()
-        elseif mod:GetStackCount() == 8 then
+        elseif mod:GetStackCount() == maxStacks then
             mod:SetStackCount(1)
         end
     end
 end
 
-
 modifier_mk_stack_debuff = class({})
 
 function modifier_mk_stack_debuff:IsDebuff() return true end
+
 function modifier_mk_stack_debuff:IsPurgable() return false end
+
 function modifier_mk_stack_debuff:GetTexture() return "monkey_king_jingu_mastery" end
 
 function modifier_mk_stack_debuff:OnCreated()
     if not IsServer() then return end
     self:SetStackCount(0)
-    self.pfxNumber = ParticleManager:CreateParticle("particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_stack.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetParent())
+    self.pfxNumber = ParticleManager:CreateParticle(
+        "particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_stack.vpcf", PATTACH_OVERHEAD_FOLLOW,
+        self:GetParent())
     self:AddParticle(self.pfxNumber, false, false, -1, false, true)
 end
 
@@ -58,7 +63,6 @@ function modifier_mk_stack_debuff:OnStackCountChanged(oldStackCount)
         ParticleManager:SetParticleControl(self.pfxNumber, 1, Vector(counter, counter, counter))
     end
 end
-
 
 function modifier_mk_stack_debuff:OnDestroy()
     if not IsServer() then return end
--- game/scripts/vscripts/modifiers/ai/modifier_mk_ai.lua
+++ game/scripts/vscripts/modifiers/ai/modifier_mk_ai.lua
@@ -7,8 +7,21 @@ function modifier_mk_ai:IsPurgable() return false end
 function modifier_mk_ai:DeclareFunctions()
     return {
         MODIFIER_EVENT_ON_ATTACK_LANDED,
-        MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+        MODIFIER_EVENT_ON_ABILITY_FULLY_CAST,
+        MODIFIER_EVENT_ON_TAKEDAMAGE,
     }
+end
+
+function modifier_mk_ai:OnTakeDamage(params)
+    local parent = self:GetParent()
+    local abil = parent:FindAbilityByName('mk_passive_custom')
+    local strike = parent:FindAbilityByName('monkey_king_boundless_strike')
+    if params.attacker == parent and params.inflictor == strike then
+        local vampMult = abil:GetSpecialValueFor('vampMult') or 1.0
+        local realHealing = vampMult * params.damage
+        parent:Heal(realHealing, abil)
+        SendOverheadEventMessage(nil, OVERHEAD_ALERT_HEAL, parent, realHealing, nil)
+    end
 end
 
 function modifier_mk_ai:OnAbilityFullyCast(params)
@@ -22,10 +35,13 @@ function modifier_mk_ai:OnAttackLanded(event)
     if not IsServer() then return end
     local attacker = event.attacker
     local victim = event.target
+    local parent = self:GetParent()
+    local abil = parent:FindAbilityByName('mk_passive_custom')
+    local maxStacks = abil:GetSpecialValueFor('max_stacks')
     if attacker == self:GetParent() then
         if victim:HasModifier('modifier_mk_stack_debuff') then
             local mod = victim:FindModifierByName('modifier_mk_stack_debuff')
-            if mod:GetStackCount() == 8 then
+            if mod:GetStackCount() == maxStacks then
                 EmitSoundOn("Bidlo.Laugh.Begin", attacker)
                 attacker.castBonk = true
             end
