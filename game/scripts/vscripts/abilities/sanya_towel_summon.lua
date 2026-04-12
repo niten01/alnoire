f5ed574093133b45b86d7af7680038352bad5fdc
niten01 <ovcharov_05@mail.ru>
2026-04-12 14:16:27 +0300

    ашч

--- game/scripts/vscripts/abilities/sanya_towel_summon.lua
+++ game/scripts/vscripts/abilities/sanya_towel_summon.lua
@@ -38,7 +38,7 @@ function sanya_towel_summon:OnSpellStart()
     caster.summon = unit
 
 
-    self:UpgradeBear(self, unit)
+    self:UpgradeBear(unit)
     unit:AddNewModifier(caster, self, "modifier_summon_distance_check", {})
     unit:SetBaseMoveSpeed(caster:GetBaseMoveSpeed())
     local pfx = ParticleManager:CreateParticle("particles/creatures/aghanim/portal_summon_b0a.vpcf",
@@ -60,6 +60,10 @@ end
 
 function sanya_towel_summon:OnHeroLevelUp()
     local caster = self:GetCaster()
+    local summon = caster.summon
+    if not summon then return end
+    self:UpgradeBear(summon)
+
     local level = caster:GetLevel()
     if TOWEL_MASTER_ULT_LEVELS[level] then
         local currentLevel = self:GetLevel()
@@ -80,22 +84,21 @@ function sanya_towel_summon:OnUpgrade()
     end
     local summon = caster.summon
     if not summon then return end
-    self:UpgradeBear(self, summon)
+    self:UpgradeBear(summon)
 end
 
 function sanya_towel_summon:GetCastRange()
     return self:GetSpecialValueFor('radius') or 1200
 end
 
-function sanya_towel_summon:UpgradeBear(ability, bear)
-    if not ability or not bear then return end
-    local ab_level = ability:GetLevel()
+function sanya_towel_summon:UpgradeBear(bear)
+    if not bear then return end
     local ab1 = bear:GetAbilityByIndex(0)
     local ab2 = bear:GetAbilityByIndex(1)
     local ab3 = bear:GetAbilityByIndex(2)
-    local targetLvl1 = ability:GetSpecialValueFor("overpower_level")
-    local targetLvl2 = ability:GetSpecialValueFor("dash_level")
-    local targetLvl3 = ability:GetSpecialValueFor("explosion_level")
+    local targetLvl1 = self:GetSpecialValueFor("overpower_level")
+    local targetLvl2 = self:GetSpecialValueFor("dash_level")
+    local targetLvl3 = self:GetSpecialValueFor("explosion_level")
     ab1:SetLevel(targetLvl1)
     ab2:SetLevel(targetLvl2)
     ab3:SetLevel(targetLvl3)
